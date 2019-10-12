76c6d816f72984b65f81e067a8573cf32ea0fd90
Giovanni D'amico <giovannidamico@Giovannis-MacBook-Pro.local>
2019-10-12 17:46:42 +0200

    relay_> 1st step

---
+++ shared/relay/FindMeGraphQlErrorDisplay.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { Icon, Text } from "native-base";
+
+const FindMeGraphQlErrorDisplay = () => {
+    return (
+        <View style={styles.view}>
+            <Icon name="alert" style={styles.icon} />
+            <Text style={styles.text}>
+                {"Oops, something went wrong!\n" +
+                    "\n" +
+                    "If this problem persists, please reach out to us by email at hello@purplesquid.scot"}
+            </Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    view: {
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    icon: {
+        color: "blue",
+    },
+    text: {
+        color: "black",
+        textAlign: "center",
+    },
+});
+
+export default FindMeGraphQlErrorDisplay;
---
+++ shared/relay/FindMeSpinner.js
@@ -0,0 +1,28 @@
+import { ActivityIndicator, StyleSheet } from "react-native";
+import PropTypes from "prop-types";
+import React from "react";
+
+const FindMeSpinner = ({ color }) => (
+    <ActivityIndicator
+        color={color}
+        size="large"
+        style={styles.activityIndicator}
+    />
+);
+
+FindMeSpinner.propTypes = {
+    color: PropTypes.string,
+};
+
+FindMeSpinner.defaultProps = {
+    color: "blue",
+};
+
+const styles = StyleSheet.create({
+    activityIndicator: {
+        flex: 1,
+        justifyContent: "flex-start",
+    },
+});
+
+export default FindMeSpinner;
