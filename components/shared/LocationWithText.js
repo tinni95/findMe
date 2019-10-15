3d40ae7aaf10530d6471046251a36f18ea74c04c
Giovanni D'amico <giovannidamico@Giovannis-MacBook-Pro.local>
2019-10-15 18:47:59 +0200

    Another fix

--- components/shared/LocationWithText.js
+++ components/shared/LocationWithText.js
@@ -6,13 +6,19 @@ export default function LocationWithText(props) {
     return (
         <View style={styles.container}>
             <Location height={20} width={20} fill={props.color} />
-            <Text style={{ marginTop: 3, marginLeft: 2, color: props.textColor }}>{props.text}</Text>
+            <Text style={[styles.text, { color: props.textColor }]}>{props.text}</Text>
         </View>
     );
 }
 const styles = StyleSheet.create({
     container: {
+        marginLeft: -3,
         flexDirection: "row",
         margin: 5
     },
+    text: {
+        fontSize: 15,
+        marginTop: 3,
+        marginLeft: 2,
+    }
 })
