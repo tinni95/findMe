6560d8a6eae3d54fb95356b5c7d8844905aceabe
Giovanni D'amico <giovannidamico@Giovannis-MacBook-Pro.local>
2019-10-16 19:30:47 +0200

    added limit to text

--- components/PostCard/PostCardText.js
+++ components/PostCard/PostCardText.js
@@ -3,13 +3,25 @@ import { StyleSheet, Text, View } from "react-native"
 import LocationWithText from "../shared/LocationWithText";
 import { Bold, Light } from "../StyledText"
 import { graphql, createFragmentContainer } from "react-relay";
+const descriptionLimit = 180;
+const titleLimit = 34;
 
-export const PostCardText = ({ post }) => {
+const fixOverflow = (text, limit) => {
+    {
+        return ((text).length > limit) ?
+            (((text).substring(0, limit - 3)) + '...') :
+            text
+    }
+}
+export const PostCardText = ({ post: { description, title } }) => {
     return (
         <View style={styles.container}>
-            <Bold>{post.title}</Bold>
+            <Bold>{fixOverflow(title, titleLimit)}</Bold>
             <LocationWithText text={"Caserta, Ce"} color={"#00B6BE"} textColor={"#ADBFC5"} />
-            <Light>{post.description}</Light>
+
+            <Light>
+                {fixOverflow(description, descriptionLimit)}
+            </Light>
         </View>
     )
 }
