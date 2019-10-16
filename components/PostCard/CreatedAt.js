36965e278e34feec94c80e40f46b10638a9ca14a
Giovanni D'amico <giovannidamico@Giovannis-MacBook-Pro.local>
2019-10-16 17:53:57 +0200

    post prepared

--- components/PostCard/CreatedAt.js
+++ components/PostCard/CreatedAt.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { StyleSheet, View } from "react-native"
+import Clock from "../../assets/images/clock.svg"
+import { Body } from "../StyledText"
 
 export default CreatedAt = () => {
     return (
         <View style={styles.container}>
-
+            <Clock
+                width={20} height={20}
+                fill={"#00B6BE"}
+            />
+            <Body style={styles.text}>1 ora fa</Body>
         </View>
     )
 }
@@ -12,6 +18,13 @@ export default CreatedAt = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 5,
+        justifyContent: "center",
+        flexDirection: "row",
+        alignItems: "center"
+    },
+    text: {
+        fontSize: 10,
+        marginLeft: 5
     }
 });
 
--- components/PostCard/Views.js
+++ components/PostCard/Views.js
@@ -2,11 +2,18 @@ import React from "react";
 import { StyleSheet, View } from "react-native"
 import { graphql, createFragmentContainer } from "react-relay";
 import { Body } from "../StyledText"
+import { Icon } from "react-native-elements";
 
-export const Views = ({ post: views }) => {
+export const Views = ({ post: { views } }) => {
     return (
         <View style={styles.container}>
-            <Body style={styles.text}>visualizzazioni</Body>
+            <Icon
+                size={20}
+                color={"#959595"}
+                name="ios-eye"
+                type="ionicon"
+            />
+            <Body style={styles.text}>{views} visualizzazioni</Body>
         </View>
     )
 }
@@ -14,10 +21,14 @@ export const Views = ({ post: views }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 3,
-        margin: 8
+        flexDirection: "row",
+        margin: 10,
+        justifyContent: "center",
+        alignItems: "center"
     },
     text: {
-        fontSize: 10
+        fontSize: 10,
+        marginLeft: 3
     }
 });
 
