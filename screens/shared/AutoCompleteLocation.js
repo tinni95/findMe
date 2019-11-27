d1010501969de3d5be011ecc9ffe0bc5f86aa0cc
Giovanni D'amico <giovannidamico@Giovannis-MacBook-Pro.local>
2019-11-27 18:29:36 +0100

    finito

--- screens/shared/AutoCompleteLocation.js
+++ screens/shared/AutoCompleteLocation.js
@@ -9,7 +9,7 @@ export function AutoCompleteLocation({ navigation }) {
     let path = navigation.getParam("path") || "";
     const [text, setText] = useState("null");
     const Input = useRef();
-    let filteredComuni = comuni.filter(comune => comune.città.toLowerCase().includes(text.toLowerCase())).slice(0, 10)
+    let filteredComuni = comuni.filter(comune => comune.città.toLowerCase().includes(text.toLowerCase())).slice(0, 25)
     const renderItems = filteredComuni.map(item => {
         return <TouchableOpacity onPress={() => navigation.navigate(path, { location: item.città + ", " + item.provincia + ", " + item.regione })} key={shortid.generate()} style={styles.item}><Light style={styles.itemText}>{item.città + ", " + item.provincia + ", " + item.regione}</Light></TouchableOpacity>
     })
