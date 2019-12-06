b906f47c5bd427a9acc6b46b41206f795b642395
Giovanni D'amico <giovannidamico@Giovannis-MacBook-Pro.local>
2019-12-06 17:23:43 +0100

    working without date

--- screens/ProfileStack/FormazioniScreen.js
+++ screens/ProfileStack/FormazioniScreen.js
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
-import { View, StyleSheet, Modal } from "react-native"
+import { View, StyleSheet, ScrollView, Modal } from "react-native"
 import WithErrorString from "../shared/Form/WithErrorString";
 import StepsLabel, { StepsLabelWithHint } from "../shared/StepsLabel";
 import FormTextInput from "../shared/Form/FormTextInput";
 import { FormStyles } from "../shared/Form/FormStyles";
 import RoundButton from "../../components/shared/RoundButton";
 import Colors from "../../constants/Colors";
-import MonthSelectorCalendar from 'react-native-month-selector';
 import { TouchableOpacity } from "react-native-gesture-handler";
 import moment from 'moment/min/moment-with-locales'
+
 moment.locale('it')
 const LINK_REGEX = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
 export default function FormazioniScreen({ navigation }) {
-    const [modalVisible, setModalVisible] = useState(false)
+    const [zoom, setZoom] = useState(false)
     const [istituto, setIstituto] = useState("")
     const [istitutoError, setIstitutoError] = useState(false)
     const [link, setLink] = useState("")
@@ -24,6 +24,7 @@ export default function FormazioniScreen({ navigation }) {
     const [dataFine, setDataFine] = useState(moment())
     const [dataFineError, setDataFineError] = useState(false)
     const [descrizione, setDescrizione] = useState("")
+    const [descrizioneError, setDescrizioneError] = useState(false)
 
     const handlePress = () => {
         if (istituto.length === 0) {
@@ -44,87 +45,72 @@ export default function FormazioniScreen({ navigation }) {
         else {
             setCorsoError(false)
         }
+        if (descrizione.length === 0) {
+            setDescrizioneError(true);
+        }
+        else {
+            setDescrizioneError(false)
+        }
     }
 
     return (
-        <View style={styles.container}>
-            <StepsLabel text={"Istituto"} />
-            <WithErrorString error={istitutoError} errorText={"Campo Obbligatorio"}>
-                <FormTextInput
-                    placeholder="Nome"
-                    onChangeText={val => setIstituto(val)}
-                    value={istituto}
-                    style={istitutoError ? FormStyles.inputError : FormStyles.input}
-                />
-            </WithErrorString>
-            <WithErrorString error={linkError} errorText={"Non è un link"}>
-                <FormTextInput
-                    placeholder="Sito Web"
-                    onChangeText={val => setLink(val)}
-                    value={link}
-                    style={istitutoError ? FormStyles.inputError : FormStyles.input}
-                />
-            </WithErrorString>
-            <View style={styles.separator}></View>
-            <StepsLabel text={"Corso Di Studi"} />
-            <WithErrorString error={corsoError} errorText={"Campo Obbligatorio"}>
-                <FormTextInput
-                    placeholder="Titolo"
-                    onChangeText={val => setCorso(val)}
-                    value={corso}
-                    style={corsoError ? FormStyles.inputError : FormStyles.input}
-                />
-            </WithErrorString>
-            <View style={FormStyles.inputHalfsContainer}>
-                <View style={FormStyles.inputHalfContainer}>
-                    <WithErrorString errorText={"Cambo Obbligatorio"}
-                        error={dataInizioError}>
-                        <TouchableOpacity onPress={() => setModalVisible(true)}>
-                            <FormTextInput
-                                pointerEvents="none"
-                                style={dataInizioError ? FormStyles.inputHalfError : FormStyles.inputHalf}
-                                placeholder="Data Inizio"
-                                placeholderTextColor="#ADADAD"
-                            />
-                        </TouchableOpacity>
+        <ScrollView style={styles.container}>
+            {!zoom &&
+                <View>
+                    <StepsLabel text={"Istituto"} />
+                    <WithErrorString error={istitutoError} errorText={"Campo Obbligatorio"}>
+                        <FormTextInput
+                            placeholder="Nome"
+                            onChangeText={val => setIstituto(val)}
+                            value={istituto}
+                            style={istitutoError ? FormStyles.inputError : FormStyles.input}
+                        />
                     </WithErrorString>
-                </View>
-                <View style={FormStyles.inputHalfContainer}>
-                    <WithErrorString errorText={"Campo Obbligatorio"} error={dataFineError}>
-                        <TouchableOpacity onPress={() => setModalVisible(true)}>
-                            <FormTextInput
-                                style={dataFineError ? FormStyles.inputHalfError : FormStyles.inputHalf}
-                                pointerEvents="none"
-                                placeholder="Data Fine"
-                                value={moment(dataFine).format("MMM YYYY")}
-                                placeholderTextColor="#ADADAD"
-                            />
-                        </TouchableOpacity>
+                    <WithErrorString error={linkError} errorText={"Non è un link"}>
+                        <FormTextInput
+                            placeholder="Sito Web"
+                            onChangeText={val => setLink(val)}
+                            value={link}
+                            style={linkError ? FormStyles.inputError : FormStyles.input}
+                        />
                     </WithErrorString>
-                </View>
-                <Modal
-                    visible={modalVisible}
-                    transparent={false}
-                    onRequestClose={() => setModalVisible(false)}>
-                    <MonthSelectorCalendar
-                        localeSettings={"es"}
-                        localeLanguage={"es"}
-                        selectedDate={dataFine}
-                        onMonthTapped={(date) => {
-                            setModalVisible(false);
-                            setDataFine(moment(date))
-                        }}
-                    />
-                </Modal>
-            </View>
+                    <View style={styles.separator}></View>
+                    <StepsLabel text={"Corso Di Studi"} />
+                    <WithErrorString error={corsoError} errorText={"Campo Obbligatorio"}>
+                        <FormTextInput
+                            placeholder="Titolo"
+                            onChangeText={val => setCorso(val)}
+                            value={corso}
+                            style={corsoError ? FormStyles.inputError : FormStyles.input}
+                        />
+                    </WithErrorString>
+                    <View style={styles.separator}></View>
+                </View>}
+            <StepsLabel error={descrizioneError} text={"Descrizione"} />
+            <FormTextInput
+                large="true"
+                multiline
+                numberOfLines={4}
+                placeholder="Titolo"
+                onChangeText={val => setDescrizione(val)}
+                value={descrizione}
+                onFocus={() => setZoom(true)}
+                onEndEditing={() => setZoom(false)}
+                textAlignVertical={"top"}
+                style={zoom ? FormStyles.xlarge : FormStyles.large}
+            />
+            {zoom && <RoundButton onPress={() => setZoom(false)} color={Colors.red} text={"OK"} textColor={"white"} />}
             <View style={styles.buttonWrapper}>
-                <RoundButton onPress={() => handlePress()} text={"CONFERMA"} color={Colors.blue} textColor="white"></RoundButton>
+                {!zoom && <RoundButton onPress={() => handlePress()} text={"CONFERMA"} color={Colors.blue} textColor="white"></RoundButton>}
             </View>
-        </View>
+        </ScrollView>
     )
 
 }
 
+FormazioniScreen.navigationOptions = {
+    title: "Aggiungi Formazione"
+}
 const styles = StyleSheet.create({
     container: { flex: 1, margin: 20 },
     buttonWrapper: {
