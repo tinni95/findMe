b2e4c9550943f7f2431704f81322f79cd4178ce1
Giovanni D'amico <giovannidamico@Giovannis-MacBook-Pro.local>
2019-12-28 16:10:07 +0100

    channel chat boilerPlate

---
+++ screens/MessaggiStack/Chat.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react'
+import { GiftedChat } from 'react-native-gifted-chat'
+
+export default function Chat() {
+    state = {
+        messages: [],
+    }
+    const [messages, setMessages] = useState([])
+
+    useEffect(() => {
+        setMessages([{
+            _id: 1,
+            text: 'Hello developer',
+            createdAt: new Date(),
+            user: {
+                _id: 2,
+                name: 'React Native',
+                avatar: 'https://placeimg.com/140/140/any',
+            },
+        }])
+    }, [])
+
+    const onSend = (message = []) => {
+        setMessages(GiftedChat.append(messages, message))
+    }
+
+    return (
+        <GiftedChat
+            messages={messages}
+            onSend={message => onSend(message)}
+            user={{
+                _id: 1,
+            }}
+        />
+    )
+}
