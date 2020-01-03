a170b64b1f4ee671b2d29497ca432c7f8974ad4a
Giovanni D'amico <giovannidamico@Giovannis-MacBook-Pro.local>
2020-01-03 22:10:00 +0100

    fixes(completed flow login)

--- screens/AuthenticationStack/LoginScreen.js
+++ screens/AuthenticationStack/LoginScreen.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { View, Text, TextInput, AsyncStorage, StyleSheet, TouchableOpacity } from 'react-native';
 import { TOKEN_KEY } from '../../shared/Token';
-import RoundButton from '../../components/shared/RoundButtonSignUpScreen';
+import RoundButtonEmpty from '../../components/shared/RoundButtonEmptySignUpScreen';
 import { isSmallDevice } from '../../constants/Layout';
 import { FormStyles } from '../shared/Form/FormStyles';
 import gql from 'graphql-tag';
@@ -9,6 +9,8 @@ import { useMutation } from '@apollo/react-hooks';
 import FindMeSpinner from "../../shared/FindMeSpinner";
 import Colors from '../../constants/Colors';
 import { Ionicons } from '@expo/vector-icons';
+import { Light } from '../../components/StyledText';
+import WithErrorString from '../shared/Form/WithErrorString';
 
 
 const LOGIN_MUTATION = gql`
@@ -35,41 +37,56 @@ export default function LoginScreen({ screenProps }) {
     const [password, setPassword] = useState("");
     const emailError = mutationError && error.message.toString().includes("user");
     const passwordError = mutationError && error.message.toString().includes("password");
-
+    let passwordInput = useRef();
     return (
         mutationLoading ?
             <FindMeSpinner />
             :
             <View style={styles.container}>
                 <View style={styles.formContainer}>
-                    <TextInput
-                        style={emailError ? FormStyles.inputError : FormStyles.input}
-                        placeholder="Email"
-                        value={email}
-                        onChangeText={email => setEmail(email)}
-                    />
-                    {emailError && <Text style={FormStyles.error}>email invalida</Text>}
-                    <TextInput
-                        autoCapitalize="none"
-                        style={passwordError ? FormStyles.inputError : FormStyles.input}
-                        placeholder="Password"
-                        secureTextEntry
-                        value={password}
-                        onChangeText={password => setPassword(password)}
-                    />
-                    {passwordError && <Text style={FormStyles.error}>password invalida</Text>}
+                    <WithErrorString
+                        error={emailError}
+                        errorString={"Email non valida"}>
+                        <TextInput
+                            style={emailError ? FormStyles.inputError : FormStyles.input}
+                            placeholder="Email"
+                            value={email}
+                            onChangeText={email => setEmail(email)}
+                            onSubmitEditing={() => passwordInput.current.focus()}
+                        />
+                    </WithErrorString>
+                    <View style={styles.spacer}></View>
+                    <WithErrorString
+                        error={passwordError}
+                        errorString={"Password non valida"}>
+                        <TextInput
+                            autoCapitalize="none"
+                            style={passwordError ? FormStyles.inputError : FormStyles.input}
+                            placeholder="Password"
+                            ref={passwordInput}
+                            secureTextEntry
+                            value={password}
+                            onChangeText={password => setPassword(password)}
+                        />
+                    </WithErrorString>
                 </View>
                 <View style={styles.buttonsContainer}>
-                    <RoundButton
+                    <View style={styles.spacer}></View>
+                    <RoundButtonEmpty
                         onPress={() => {
                             let emails = email.toString().toLowerCase();
                             login({ variables: { email: emails, password } });
                         }}
                         isLong
-                        fontColor={Colors.red}
-                        text="ACCEDI"
-                        color={Colors.red}
+                        isMedium
+                        fontColor={Colors.blue}
+                        text="Accedi"
+                        color={Colors.blue}
                     />
+                    <View style={styles.spacer}></View>
+                    <TouchableOpacity style={{ alignContent: "center" }}>
+                        <Light style={{ color: Colors.blue }}>Password Dimenticata?</Light>
+                    </TouchableOpacity>
                 </View>
             </View>
     );
@@ -91,15 +108,16 @@ const styles = StyleSheet.create({
         marginTop: 20
     },
     formContainer: {
-        margin: isSmallDevice ? 30 : 40,
-        marginTop: isSmallDevice ? 40 : 60,
+        margin: 30,
+        marginTop: isSmallDevice ? 20 : 40,
         justifyContent: 'center'
     },
     buttonsContainer: {
         flex: isSmallDevice ? 10 : 7.5,
         alignItems: 'center',
         justifyContent: 'flex-start'
-    }
+    },
+    spacer: { height: 20 }
 });
 
 LoginScreen.navigationOptions = ({ navigation }) => {
