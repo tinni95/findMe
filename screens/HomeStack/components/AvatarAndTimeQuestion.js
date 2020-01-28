0c1372d6f9b41b189cc6a247113730a734adfec5
Giovanni D'amico <giovannidamico@Giovannis-MacBook-Pro.local>
2020-01-28 20:04:43 +0100

    image

--- screens/HomeStack/components/AvatarAndTimeQuestion.js
+++ screens/HomeStack/components/AvatarAndTimeQuestion.js
@@ -7,10 +7,9 @@ moment.locale('it');
 export default AvatarAndTimeQuestion = ({ question, text }) => {
     const image = question.postedBy.pictureUrl ? { uri: question.postedBy.pictureUrl } : require("../../../assets/images/placeholder.png")
     return (
-
         <View style={styles.header}>
             <View style={styles.imageContainer}>
-                <Image source={require(image)} style={{ width: 50, height: 50, borderRadius: 25 }} />
+                <Image source={image} style={{ width: 50, height: 50, borderRadius: 25 }} />
             </View>
             <View style={styles.content}>
                 <Body style={styles.person}>{question.postedBy.nome + " " + question.postedBy.cognome}</Body>
