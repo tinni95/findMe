fe9da63ea773e3ada075472f273537a0c9e24abc
Giovanni D'amico <giovannidamico@Giovannis-MacBook-Pro.local>
2020-05-05 22:00:23 +0200

    redundant code

--- navigation/Icons/CreateIcon.tsx
+++ navigation/Icons/CreateIcon.tsx
@@ -1,30 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 
-import { StyleSheet, View, Image } from "react-native";
-import { Body, Bold } from "../../shared/components/StyledText";
+import {  View, Image } from "react-native";
+import {  Bold } from "../../shared/components/StyledText";
 import Colors from "../../shared/constants/Colors";
-import { useQuery, useMutation } from "react-apollo";
-import { gql } from "apollo-boost";
-import SocketContext from "../../shared/SocketContext";
-import { wait } from "../../shared/functions/wait";
-
-const UNOPENEDNOTIFICHE_QUERY = gql`
-  {
-    UnreadNotifiche {
-      opened
-    }
-    currentUser {
-      id
-    }
-  }
-`;
-const READNOTIFICA_MUTATION = gql`
-  mutation {
-    viewNotificas {
-      count
-    }
-  }
-`;
 
 function CreateIcon(props) {
   if (props.focused) {
@@ -71,24 +49,5 @@ function CreateIcon(props) {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: "row",
-    marginRight: -5,
-    paddingBottom: 5
-  },
-  counter: {
-    height: 13,
-    width: 13,
-    borderRadius: 6.5,
-    marginLeft: -8,
-    backgroundColor: Colors.red
-  },
-  text: {
-    textAlign: "center",
-    color: "white",
-    fontSize: 9
-  }
-});
 
 export default CreateIcon;
